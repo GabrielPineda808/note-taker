402a7b425426cda45595413987689bdf3f1306d9
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-16 20:11:41 -0700

    creating the routes that will lead to the correct files and join them

--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,15 @@ var PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
