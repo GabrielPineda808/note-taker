8e8e044299577048af412804bdc74434983c5736
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-16 20:02:21 -0700

    removed the routes folder and will continue all the work i nthe server.js file as i could not get the pathing correct

--- Develop/routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-var note = require("../db/notes.js");
-
-app.get("/notes", function(req,res){
-    note.getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => console.log(err))
-})
